be7e7773d7cac1d7e8440a37c482c5c2a283d44b
Reza Asghari <rezaasghari.m.b@gmail.com>
2023-11-20 17:10:16 +0330

    complete the app logic

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
 'use strict';
 
-const birthDay = Number(document.getElementById('input-date--day').value);
-const birthMonth = Number(document.getElementById('input-date--month').value);
-const birthYear = Number(document.getElementById('input-date--year').value);
+let birthDay = document.getElementById('input-date--0');
+let birthMonth = document.getElementById('input-date--1');
+let birthYear = document.getElementById('input-date--2');
 
-const calcDay = document.getElementById('output-date--day');
-const calcMonth = document.getElementById('output-date--month');
-const calcYear = document.getElementById('output-date--year');
+let calcDay = document.getElementById('output-date--day');
+let calcMonth = document.getElementById('output-date--month');
+let calcYear = document.getElementById('output-date--year');
 
 const currentDate = new Date();
 const currDay = currentDate.getDate();
 const currMonth = currentDate.getMonth() + 1;
 const currYear = currentDate.getFullYear();
 
-const birthDate = [birthDay,birthMonth,birthYear];
+let birthDate = [Number(birthDay.value),Number(birthMonth.value),Number(birthYear.value)];
 const currDate = [currDay,currMonth,currYear];
-const calcDate = [calcDay,calcMonth,calcYear];
+let calcDate = [calcDay,calcMonth,calcYear];
 
 const btn = document.querySelector('.btn');
+
+const init = function() {
+    birthDay.value = null;
+    birthMonth.value = null;
+    birthYear.value = null;
+    birthDate = [0,0,0];
+}
+
+const colorChanger = function(i){
+    document.getElementById(`label--${i}`).style.color = '#e47b7f';
+    document.getElementById(`input-date--${i}`).style.borderColor = '#e47b7f';
+}
+const colorReset = function(i){
+    document.querySelector(`.input-date__valid--${i}`).textContent = '';
+    document.getElementById(`label--${i}`).style.color = 'var(--Smokey-grey)';
+    document.getElementById(`input-date--${i}`).style.borderColor = 'var(--Light-grey)';
+}
+
+const dayToArray = function(e) {
+    birthDate[0] = Number(e);
+}
+const monthToArray = function(e) {
+    birthDate[1] = Number(e);
+}
+const yearToArray = function(e) {
+    birthDate[2] = Number(e);
+}
 
 const resolveDateIssues = function () {
     for (let i = 0 ; i < birthDate.length - 1; i++) {
@@ -31,13 +58,64 @@ const resolveDateIssues = function () {
     }
 }
 
-resolveDateIssues();
-btn.addEventListener('click', function() {   
-    for (let i = 0; i < birthDate.length; i++) {
-        calcDate[i] = currDate[i] - birthDate[i];
-    }
 
-    calcDay.textContent = calcDate[0];
-    calcMonth.textContent = calcDate[1];
-    calcYear.textContent = calcDate[2];
+const emptyCheck = function() {
+    let conditionCheck = false;
+    for (let i = 0; i < birthDate.length ; i++) {
+        if (birthDate[i] === 0 || birthDate === NaN) {
+            document.querySelector(`.input-date__valid--${i}`).textContent = 'This field is required';
+            colorChanger(i);
+            conditionCheck = true;
+        } else {
+            colorReset(i);
+        }
+    }
+    return conditionCheck;
+}
+
+const overDate = function () {
+    let conditionCheck = false;
+    for (let i = 0 ; i < birthDate.length; i++) {
+        if (birthDate[i] > 31 && i === 0){
+            document.querySelector(`.input-date__valid--${i}`).textContent = 'Must be a valid day';
+            colorChanger(i);
+            conditionCheck = true;
+        } else if (birthDate[i] > 12 && i == 1){
+            document.querySelector(`.input-date__valid--${i}`).textContent = 'Must be a valid month';
+            colorChanger(i);
+            conditionCheck = true;
+        } else if (birthDate[i] > currDate[i] && i === 2){
+            document.querySelector(`.input-date__valid--${i}`).textContent = 'Must be a valid year';
+            colorChanger(i);
+            conditionCheck = true;
+        } else {
+            colorReset(i);
+        }
+    }
+    return conditionCheck;
+}
+
+
+btn.addEventListener('click', function() {
+    
+    emptyCheck();
+    overDate();
+    console.log(emptyCheck());
+    console.log(overDate());
+    if (emptyCheck() === false && overDate() === false) {
+        resolveDateIssues();
+        for (let i = 0 ; i < birthDate.length; i++) {
+            calcDate[i] = currDate[i] - birthDate[i];
+        }
+        calcDay.textContent = calcDate[0];
+        calcMonth.textContent = calcDate[1];
+        calcYear.textContent = calcDate[2];
+    } else {
+        calcDay.textContent = '--';
+        calcMonth.textContent = '--';
+        calcYear.textContent = '--';
+    }
+            
 })
+
+init();
